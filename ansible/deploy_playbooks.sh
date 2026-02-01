e760a421671a69dc8ef73d2ce5650c77d7b93374
Andrzej Waloszek <waloszek.andrzej@gmail.com>
2026-02-01 21:59:54 +0100

    ansible master playbooks

--- ansible/deploy_playbooks.sh
+++ ansible/deploy_playbooks.sh
@@ -1,5 +1,5 @@
 #ansible all -m ping
-#ansible-playbook -e @group_vars/all.yml -i inventory/hosts.yml playbooks/000-setup-raspi.yml
+#ansible-playbook -e @group_vars/all.yml -i inventory/hosts.yml playbooks/000-setup-raspi.yml --vault-password-file ansible/.vaultpass
 #ansible-playbook -e @group_vars/all.yml -i inventory/hosts.yml playbooks/0010setup-k3s.yml#ansible-playbook -e @group_vars/all.yml -i inventory/hosts.yml playbooks/003a_deploy-nfs-storage.yml
 #ansible-playbook -e @group_vars/all.yml -i inventory/hosts.yml playbooks/003a-deploy-nfs-storage.yml
 #ansible-playbook -e @group_vars/all.yml -i inventory/hosts.yml playbooks/003b-deploy-longhorn-storage.yml
@@ -7,4 +7,7 @@
 #ansible-playbook -e @group_vars/all.yml -i inventory/hosts.yml playbooks/005-deploy-metallb-nginx-ingress.yml
 #ansible-playbook -e @group_vars/all.yml -i inventory/hosts.yml playbooks/006-deploy-monitoring.yml
 #ansible-playbook -e @group_vars/all.yml -i inventory/hosts.yml playbooks/007-deploy-logging.yml
-ansible-playbook -e @group_vars/all.yml -i inventory/hosts.yml playbooks/008-deploy-sealed-secrets.yml # --vault-password-file secrets/.vaultpass
+#ansible-playbook -e @group_vars/all.yml -i inventory/hosts.yml playbooks/008-deploy-sealed-secrets.yml # --vault-password-file secrets/.vaultpass
+
+
+#
